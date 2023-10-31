22099022caa932d2abbb4f6e350958f70ea3743c
mahcopration <129929195+mahcopration@users.noreply.github.com>
2023-10-31 23:23:08 +0530

    Update start.py

--- SankiMusic/utilities/inline/start.py
+++ SankiMusic/utilities/inline/start.py
@@ -44,7 +44,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="✮ sᴏᴜʀᴄᴇ ✮", url="https://github.com/XdityaHalder/SankiMusic"
+                text="✮ sᴏᴜʀᴄᴇ ✮", url="https://t.me/Cinemas_Trend/3"
             )
         ]
      ]
